742a4bbda5e52fd75e3fbab156efd24fb28e7926
Darya Sanina <59654064+DaryaSanina@users.noreply.github.com>
2021-10-20 18:49:38 +0300

    Add text editor

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap, QFont
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QInputDialog, QFileDialog, QMessageBox
 from PyQt5.QtWidgets import QStackedWidget
+
+HTML_EXTENSIONS = ['.htm', '.html']
+TEXT_EXTENSIONS = ['.txt']
+IMAGE_EXTENSIONS = ['.png', '.jpg', '.bmp']
+AUDIO_EXTENSIONS = ['.mp3', '.wav']
+VIDEO_EXTENSIONS = ['.mp4', '.mov', '.wmv', '.avi']
 
 filename = ''
 
@@ -44,21 +50,23 @@ class MainWindow(QMainWindow):
         extension = filename[filename.rfind('.')::]
 
         # Guessing what editor the user wants to open
-        if extension == '.txt' \
+        if extension in TEXT_EXTENSIONS or extension in HTML_EXTENSIONS \
                 and QMessageBox.question(self, '', "Do you want to edit a text document?",
                                          QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
             windows.setCurrentIndex(1)
+            text_editing_window.filename = filename
+            with open(filename, 'r', encoding='utf-8') as source_file:
+                text_editing_window.text_edit.setText(source_file.read())
 
-        elif (extension == '.png' or extension == '.jpg' or extension == '.bmp') \
+        elif extension in IMAGE_EXTENSIONS \
                 and QMessageBox.question(self, '', "Do you want to edit an image?",
                                          QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
             windows.setCurrentIndex(2)
-        elif (extension == '.mp3' or extension == '.wav') \
+        elif extension in AUDIO_EXTENSIONS \
                 and QMessageBox.question(self, '', "Do you want to edit an audio file?",
                                          QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
             windows.setCurrentIndex(3)
-        elif (extension == '.mp4' or extension == '.mov' or extension == '.wmv'
-                or extension == '.avi') \
+        elif extension in VIDEO_EXTENSIONS \
                 and QMessageBox.question(self, '', "Do you want to edit a video?",
                                          QMessageBox.Yes | QMessageBox.No) == QMessageBox.Yes:
             windows.setCurrentIndex(4)
@@ -75,6 +83,9 @@ class MainWindow(QMainWindow):
             if ok_pressed:
                 if file_type == "Text":
                     windows.setCurrentIndex(1)
+                    text_editing_window.filename = filename
+                    with open(filename, 'r', encoding='utf-8') as source_file:
+                        text_editing_window.text_edit.setText(source_file.read())
 
                 elif file_type == "Image":
                     windows.setCurrentIndex(2)
@@ -86,8 +97,95 @@ class MainWindow(QMainWindow):
 
 
 class TextEditingWindow(QMainWindow):
-    # TODO
-    pass
+    def __init__(self):
+        super(TextEditingWindow, self).__init__()
+        uic.loadUi('designs/text_editor_window.ui', self)
+
+        self.new_btn.clicked.connect(self.new)
+        self.open_btn.clicked.connect(self.open)
+        self.save_btn.clicked.connect(self.save)
+        self.save_as_btn.clicked.connect(self.save_as)
+
+        self.font_combo_box.currentFontChanged.connect(self.change_font)
+
+        self.font_size_spin_box.valueChanged.connect(self.change_font_point_size)
+
+        self.bold_btn.clicked.connect(self.set_bold)
+        self.italic_btn.clicked.connect(self.set_italic)
+        self.underline_btn.clicked.connect(self.set_underlined)
+        self.strikeout_btn.clicked.connect(self.set_strikeout)
+
+    def new(self):
+        global filename
+
+        self.text_edit.setText('')
+        filename = ''
+
+    def open(self):
+        global filename
+
+        filename = QFileDialog.getOpenFileName(self, 'Choose file', '')[0]
+        with open(filename, 'r', encoding='utf-8') as source_file:
+            self.text_edit.setText(source_file.read())
+
+    def save(self):
+        global filename
+
+        if filename == '':
+            # If the file is new, ask the user for the filename
+            filename = QFileDialog.getSaveFileName(self, 'Save file', '')[0]
+        with open(filename, 'w', encoding='utf-8') as dest_file:
+            extension = filename[filename.rfind('.')::]
+            # If the extension of the file the user wants to save the text to is an html extension,
+            # save the text with formatting,
+            # else save the text without formatting
+            text = self.text_edit.toHtml() if extension in HTML_EXTENSIONS \
+                else self.text_edit.toPlainText()
+            dest_file.write(text)
+
+    def save_as(self):
+        global filename
+
+        filename = QFileDialog.getSaveFileName(self, 'Save file', '')[0]
+        with open(filename, 'w', encoding='utf-8') as dest_file:
+            dest_file.write(self.text_edit.toPlainText())
+
+    def change_font(self, font):
+        self.text_edit.setCurrentFont(font)
+
+    def change_font_point_size(self, font_point_size):
+        self.text_edit.setFontPointSize(int(font_point_size))
+
+    def set_bold(self):
+        # Make the text bold if the "bold_btn" is checked, else make it not bold
+        if self.sender().isChecked():
+            self.text_edit.setFontWeight(QFont.Bold)
+        else:
+            self.text_edit.setFontWeight(QFont.NoFontMerging)
+
+    def set_italic(self):
+        # Make the text italic if the "italic_btn" is checked, else make it not italic
+        if self.sender().isChecked():
+            self.text_edit.setFontItalic(True)
+        else:
+            self.text_edit.setFontItalic(False)
+
+    def set_underlined(self):
+        # Underline the text if the "underline_btn" is checked, else make it not underlined
+        if self.sender().isChecked():
+            self.text_edit.setFontUnderline(True)
+        else:
+            self.text_edit.setFontUnderline(False)
+
+    def set_strikeout(self):
+        # Strikeout the text if the "strikeout_btn" is checked, else make it not stroke out
+        font = self.text_edit.currentFont()
+        if self.sender().isChecked():
+            font.setStrikeOut(True)
+            self.text_edit.setCurrentFont(font)
+        else:
+            font.setStrikeOut(False)
+            self.text_edit.setCurrentFont(font)
 
 
 class ImageEditingWindow(QMainWindow):
