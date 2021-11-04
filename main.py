80567076e642b8ea50b8e8d3258fcb4093494eb0
darya_sanina <dasha.sanina.07@gmail.com>
2021-11-04 19:51:14 +0300

    Add type hints for methods

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ class MainWindow(QMainWindow):
         self.create_new_btn.clicked.connect(self.choose_what_to_create)
         self.open_btn.clicked.connect(self.open)
 
-    def initUI(self):
+    def initUI(self) -> None:
         # Displaying the logo in the main window
         self.title_image_pixmap = QPixmap('designs/title_image.jpg')
         self.title_image = QLabel(self)
@@ -30,7 +30,7 @@ class MainWindow(QMainWindow):
         self.title_image.resize(480, 125)
         self.title_image.setPixmap(self.title_image_pixmap)
 
-    def choose_what_to_create(self):
+    def choose_what_to_create(self) -> None:
         # Creating an input dialog to ask the user what editor to open
         file_type, ok_pressed = QInputDialog.getItem(self, "Choose media type",
                                                      "Choose what you want to create:",
@@ -42,7 +42,7 @@ class MainWindow(QMainWindow):
             elif file_type == "Text":
                 windows.setCurrentIndex(1)
 
-    def open(self):
+    def open(self) -> None:
         # Getting the filename and the file extension
         global filename
         filename = QFileDialog.getOpenFileName(self, 'Choose file', '')[0]
@@ -110,20 +110,20 @@ class TextEditingWindow(QMainWindow):
         self.underline_btn.clicked.connect(self.set_underlined)
         self.strikeout_btn.clicked.connect(self.set_strikeout)
 
-    def new(self):
+    def new(self) -> None:
         global filename
 
         self.text_edit.setText('')
         filename = ''
 
-    def open(self):
+    def open(self) -> None:
         global filename
 
         filename = QFileDialog.getOpenFileName(self, 'Choose file', '')[0]
         with open(filename, 'r', encoding='utf-8') as source_file:
             self.text_edit.setText(source_file.read())
 
-    def save(self):
+    def save(self) -> None:
         global filename
 
         if filename == '':
@@ -138,41 +138,41 @@ class TextEditingWindow(QMainWindow):
                 else self.text_edit.toPlainText()
             dest_file.write(text)
 
-    def save_as(self):
+    def save_as(self) -> None:
         global filename
 
         filename = QFileDialog.getSaveFileName(self, 'Save file', '')[0]
         with open(filename, 'w', encoding='utf-8') as dest_file:
             dest_file.write(self.text_edit.toPlainText())
 
-    def change_font(self, font):
+    def change_font(self, font: QFont) -> None:
         self.text_edit.setCurrentFont(font)
 
-    def change_font_point_size(self, font_point_size):
-        self.text_edit.setFontPointSize(int(font_point_size))
+    def change_font_point_size(self, font_point_size: int) -> None:
+        self.text_edit.setFontPointSize(font_point_size)
 
-    def set_bold(self):
+    def set_bold(self) -> None:
         # Make the text bold if the "bold_btn" is checked, else make it not bold
         if self.sender().isChecked():
             self.text_edit.setFontWeight(QFont.Bold)
         else:
             self.text_edit.setFontWeight(QFont.NoFontMerging)
 
-    def set_italic(self):
+    def set_italic(self) -> None:
         # Make the text italic if the "italic_btn" is checked, else make it not italic
         if self.sender().isChecked():
             self.text_edit.setFontItalic(True)
         else:
             self.text_edit.setFontItalic(False)
 
-    def set_underlined(self):
+    def set_underlined(self) -> None:
         # Underline the text if the "underline_btn" is checked, else make it not underlined
         if self.sender().isChecked():
             self.text_edit.setFontUnderline(True)
         else:
             self.text_edit.setFontUnderline(False)
 
-    def set_strikeout(self):
+    def set_strikeout(self) -> None:
         # Strikeout the text if the "strikeout_btn" is checked, else make it not stroke out
         font = self.text_edit.currentFont()
         if self.sender().isChecked():
